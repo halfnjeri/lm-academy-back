b96dd029fe2732e1304a0902c0ea0a867be8a1d2
suelaBerisha <berishasuela60@gmail.com>
2025-03-18 23:38:53 -0700

    UserInfomModel

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\UserInfo;
 //ss
 class User extends Authenticatable
 {
@@ -52,4 +53,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function userInfo()
+    {
+        return $this->hasOne(UserInfo::class,'user_id','id');
+    } 
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserListSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserListSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
