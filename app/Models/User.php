35e7800c2d8328dbf0ea5e4b3419f4c31315020f
suelaBerisha <berishasuela60@gmail.com>
2025-03-19 18:08:54 -0700

    Fixed some issues from last push & Course model with  msc completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,19 +56,35 @@ class User extends Authenticatable
     }
     public function scopeSelectSomeUserData ($query){
         return $query->select('users.id','first_name','last_name', 'gender','email','image');
-}
+    }
+    public function scopeSelectUserName ($query){
+        return $query->select('users.id','first_name','last_name');
+    }
+
     public function userInfo()
     {
         return $this->hasOne(UserInfo::class,'user_id','id');
     } 
 
-    public function userLists()
+    public function lists()
     {
         return $this->belongsToMany(UserList::class, 'user_list_item', 'user_id', 'list_id')->withTimeStamps(); 
     }
 
     public function scoreOnScoreboard()
     {
-        $this->hasOne(Scoreboard::class,'user_id');
+        return $this->hasOne(Scoreboard::class,'user_id');
+    }
+
+    //-------------------------courses-------------------------//
+
+    public function createdCourses()
+    {
+        return $this->hasMany(Course::class,'created_by');
+    }
+
+    public function updatedCourses()
+    {
+        return $this->hasMany(Course::class,'updated_by');
     }
 }
