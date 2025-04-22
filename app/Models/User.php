aea578675de40f23b88ac23af51c8ffc504323a6
suelaBerisha <berishasuela60@gmail.com>
2025-04-22 16:14:09 +0200

    installing api on our Laravel Project

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Course;
+use App\Models\UserInfo;
 use App\Models\UserList;
+use App\Models\Scoreboard;
+use App\Models\CourseMaterial;
+use laravel\sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\UserInfo;
+
 //sss
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
