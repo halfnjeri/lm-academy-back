6a6c771b9ba040c6bcc83d85ee6a703b642099d1
suelaBerisha <berishasuela60@gmail.com>
2025-05-03 19:50:25 +0200

    Auth is done

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,14 @@ use App\Models\UserInfo;
 use App\Models\UserList;
 use App\Models\Scoreboard;
 use App\Models\CourseMaterial;
-use laravel\sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 //sss
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
@@ -46,6 +47,26 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * Get the attributes that should be cast.
